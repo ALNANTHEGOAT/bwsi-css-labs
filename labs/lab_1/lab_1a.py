515d48967e1c79648dede60b8cdab2ddc3920c39
ALNANTHEGOAT <tharrison0312@icloud.com>
2026-03-24 09:02:14 -0500

    ADDED INTRODUCTION TO LAB 1A

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Tristan" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Sup peeps, my name is Tristan and I am a junior currently attending Booker T. Washington High school in Tulsa, Oklahoma. A fun fact about me is that I love backpacking, this past summer I backpacked 100 miles through Yellowstone over a span of 30 days with NOLS. I'm in the process of applying for the AUV summer course, I was contemplating RaceCar and AAVR, but I figured getting our of my comfort zone and exploring a more exotic form of engineering and robotic systems would be more compelling. I am excited to be working with such ambitious and intelligent peers, I hope to learn and teach as much as possible during the duration of this program!")
 
 if __name__ == "__main__":
     main()
